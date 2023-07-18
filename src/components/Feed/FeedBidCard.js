9e0c9451d75d0df09cd174f5efc142747ac88503
Väinö Rantanen <vaino01@hotmail.fi>
2023-07-18 18:52:07 +0300

    korjattu tarjouksen tekemisen ominaisuus

--- src/components/Feed/FeedBidCard.js
+++ src/components/Feed/FeedBidCard.js
@@ -1,12 +1,21 @@
-import { Container, Typography } from '@mui/material'
+import { Box, Typography } from '@mui/material'
 import React from 'react'
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 
 const FeedBidCard = ({ bid }) => {
+  console.log("biddi: ", bid)
   return (
-    <Container>
-      <Typography>Hinta: {bid.price}</Typography>
+    <Box sx={{ backgroundColor: 'white', borderRadius: '1rem', padding: '1rem', marginBottom: '1rem' }}>
+      {bid.isApproved && (
+        <Box>
+          <Typography>Hyväksytty</Typography>
+          <CheckCircleIcon />
+        </Box>
+      )}
+      <Typography>Hinta: {bid.price} euroa</Typography>
       <Typography>Selite: {bid.description}</Typography>
-    </Container>
+      <Typography>Tarjous jätetty: {bid.timeStamp}</Typography>
+    </Box>
   )
 }
 
