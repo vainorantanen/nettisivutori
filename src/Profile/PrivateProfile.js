39a670e5eb8a542575dd66b06001cf9d7e37ff42
Väinö Rantanen <vaino01@hotmail.fi>
2023-07-23 22:17:04 +0300

    lisätty ominaisuus ilmoituksen sulkemiselle

--- src/Profile/PrivateProfile.js
+++ src/Profile/PrivateProfile.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react'
-import { Container, Typography, Box } from '@mui/material'
+import { Container, Typography, Box, Button } from '@mui/material'
 import CompanyProfileInfo from './CompanyProfileInfo'
 import feedpostService from '../services/feedposts'
 import FeedPostCard from '../components/Feed/FeedPostCard'
 import Togglable from '../components/Togglable'
 import ModifyPost from './ModifyPost'
+import FeedBidCard from '../components/Feed/FeedBidCard'
 
 const PrivateProfile = ({ user, setUser }) => {
   const [userFeedPosts, setuserFeedPosts] = useState([])
@@ -18,6 +19,22 @@ const PrivateProfile = ({ user, setUser }) => {
     fetchuserFeedPosts()
   }, [user.id])
 
+  const handleMarkDone = async ({ post }) => {
+    const confirmed = window.confirm('Haluatko varmasti merkitä tämän ilmoituksen suljetuksi?')
+    if (!confirmed) {
+      return // If the user clicks "Cancel," do nothing
+    }
+    await feedpostService.update({ ...post, isOpen: false })
+  }
+
+  const handleMarkOpen = async ({ post }) => {
+    const confirmed = window.confirm('Haluatko varmasti merkitä tämän ilmoituksen avoimeksi?')
+    if (!confirmed) {
+      return // If the user clicks "Cancel," do nothing
+    }
+    await feedpostService.update({ ...post, isOpen: true })
+  }
+
   if (!user) {
     return null
   }
@@ -28,11 +45,35 @@ const PrivateProfile = ({ user, setUser }) => {
       <CompanyProfileInfo user={user} setUser={setUser} />
       <Typography>Seuraa ilmoitustesi tilannetta</Typography>
       {userFeedPosts.map(m => (
-        <Box key={m.id} sx={{ marginTop: '1rem' }}>
+        <Box key={m.id} sx={{ marginTop: '1rem', border: '1px solid black', borderRadius: '1rem' }}>
           <FeedPostCard post={m}/>
-          <Togglable buttonLabel='Muokkaa'>
+          <Togglable buttonLabel='Muokkaa ilmoituksen sisältöä'>
             <ModifyPost post={m}/>
           </Togglable>
+          {m.isOpen ? (
+            <Button
+              onClick={() => handleMarkDone({ post: m })}
+            >
+            Merkitse ilmoitus suljetuksi
+            </Button>
+          ): (
+            <Button
+              onClick={() => handleMarkOpen({ post: m })}
+            >
+            Merkitse ilmoitus avoimeksi
+            </Button>
+          )}
+          {m.feedBids.map(b => (
+            <Box key={b.id}>
+              <FeedBidCard bid={b}/>
+              <Button>
+                Hyväksy tarjous
+              </Button>
+              <Button>
+                Hylkää tarjous
+              </Button>
+            </Box>
+          ))}
         </Box>
       ))}
     </Container>
